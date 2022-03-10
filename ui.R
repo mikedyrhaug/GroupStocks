0b3781ffddcbac8dfdecbca9a8e92786f072a92f
ChrisHishmeh <98780864+ChrisHishmeh@users.noreply.github.com>
2022-03-10 09:16:18 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ stocks <- read_csv("nyse_stocks.csv.zip")
 ui <- fluidPage(
   navbarPage("Stocks",
              tabPanel("Best Performing Stock",
-                      dateRangeInput("dates", label = "Date range", 
+                      dateRangeInput("dates", label = "Input  date range to find the best performing stock in that time period", 
                                      start = min(stock$Date),
                                      end=max(stock$Date)),
                       dataTableOutput("max")))
