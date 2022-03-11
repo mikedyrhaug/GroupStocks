275f8865bb74eb5b8bf881d8ff41aa04b33271e6
ChrisHishmeh <98780864+ChrisHishmeh@users.noreply.github.com>
2022-03-11 11:12:15 -0500

    Update scratchpad.R

--- scratchpad.R
+++ scratchpad.R
@@ -1,6 +1,6 @@
 library(fpp3)
 library(readr)
-
+library(seasonal)
 # Read zipped data
 stocks <- read_csv("nyse_stocks.csv.zip")
 
@@ -22,3 +22,90 @@ selected_stocks <- c("GOOG", "AAPL")
 stocks %>%
   filter(symbol %in% selected_stocks) %>%
   autoplot(open)
+
+
+
+
+#feature 2
+
+ui <- fluidPage(
+  radioButtons("sec", label = "select a sector",
+               choices = unique(stocks$gics_sector) 
+               ),
+  dateRangeInput("d", label= "insert a date", 
+                 start=min(stocks$date), 
+                 end=max(stocks$date)),
+  dataTableOutput("tab2")
+)
+  
+  
+  
+  
+server<-function(input,output){
+  output$tab2 <- renderDataTable({
+    p <- filter(stocks, stocks$gics_sector==input$sec)
+    first_date <- input$d[1]
+    last_date <- input$d[2]
+    first_day <- p %>%
+      filter(date == first_date)
+    last_day <- p %>%
+      filter(date == last_date)
+    first_last_day <- first_day %>%
+      inner_join(
+        last_day,
+        by = "symbol",
+        suffix = c("First", "Last"))
+    f <- first_last_day %>%
+      mutate(PercentChange = (openLast - openFirst) / openFirst) %>%
+      mutate(PercentChangeStr = paste0(round(PercentChange * 100, 1), "%")) %>%
+      select(symbol, openFirst, openLast, PercentChange, PercentChangeStr)
+    f<-as.data.frame(f[which.max(f$PercentChange),])
+    f
+  })
+}
+
+shinyApp(ui,server)
+#feature3
+ui <- fluidPage(
+  checkboxGroupInput("CheckGroup", label = h3("Checkbox group"), 
+                     choices = unique(stocks$gics_sector)),
+  plotOutput("tab3")
+)
+
+
+server <- function(input,output){
+  output$tab3 <- renderPlot({
+    ag <- aggregate(close~gics_sector+date, stocks, FUN = mean)
+    ag <- tsibble(ag, index=date, key = gics_sector)
+    ag <- filter(ag, ag$gics_sector%in%input$CheckGroup)
+    autoplot(ag)
+  })
+}
+
+#feature 4
+
+ui <- fluidPage(
+  numericInput("num", label = "How much money would I have if I bought this many shares", value = 1),
+  dateRangeInput("date", label= "Buying and selling on these dates", 
+                 start=min(stocks$date), 
+                 end=max(stocks$date)),
+  textInput("text", 
+            label = "Of this company", 
+            value = "Netflix Inc."),
+  textOutput("tab4")
+)
+
+server <- function(input, output){
+  output$tab4 <- renderPrint({
+    number <- stocks[stocks$security==input$text,]
+    first_day <- number[number$date==input$date[1],]
+    last_day <- number[number$date==input$date[2],]
+    dolla <- (last_day$open*input$num)-(first_day$open*input$num)
+    paste("$", round(dolla,2))
+ })
+}
+shinyApp(ui,server)
+
+
+
+
