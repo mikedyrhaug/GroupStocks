ad2cc2a00a4d37884d60185c86947d563906fbad
hdw34bp <82785244+hdw34bp@users.noreply.github.com>
2022-03-11 12:19:54 -0500

    Combined Features

--- server.R
+++ server.R
@@ -18,7 +18,46 @@ server <- function(input, output){
     
     f<-as.data.frame(f[which.max(f$PercentChange),])
     f
-
+    
+  })
+  
+  #Feature 2
+  output$tab2 <- renderDataTable({
+    p <- filter(stocks, stocks$gics_sector==input$sec)
+    first_date <- input$d[1]
+    last_date <- input$d[2]
+    first_day <- p %>%
+      filter(date == first_date)
+    last_day <- p %>%
+      filter(date == last_date)
+    first_last_day <- first_day %>%
+      inner_join(
+        last_day,
+        by = "symbol",
+        suffix = c("First", "Last"))
+    f <- first_last_day %>%
+      mutate(PercentChange = (openLast - openFirst) / openFirst) %>%
+      mutate(PercentChangeStr = paste0(round(PercentChange * 100, 1), "%")) %>%
+      select(symbol, openFirst, openLast, PercentChange, PercentChangeStr)
+    f<-as.data.frame(f[which.max(f$PercentChange),])
+    f
+  })
+  
+  #Feature 3
+  output$tab3 <- renderPlot({
+    ag <- aggregate(close~gics_sector+date, stocks, FUN = mean)
+    ag <- tsibble(ag, index=date, key = gics_sector)
+    ag <- filter(ag, ag$gics_sector%in%input$CheckGroup)
+    autoplot(ag)
+  })
+  
+  #Feature 4
+  output$tab4 <- renderPrint({
+    number <- stocks[stocks$security==input$text,]
+    first_day <- number[number$date==input$date[1],]
+    last_day <- number[number$date==input$date[2],]
+    dolla <- (last_day$open*input$num)-(first_day$open*input$num)
+    paste("$", round(dolla,2))
   })
 }  
 
--- ui.R
+++ ui.R
@@ -2,17 +2,110 @@ library(shiny)
 library(shinythemes)
 library(readr)
 library(fpp3)
+library(DT)
 
 stocks <- read_csv("nyse_stocks.csv.zip")
 
 ui <- fluidPage(
   navbarPage("Stocks",
+             #Feature 1
              tabPanel("Best Performing Stock",
                       dateRangeInput("dates", label = "Input  date range to find the best performing stock in that time period", 
                                      start = min(stocks$date),
                                      end=max(stocks$date)),
-                      dataTableOutput("max")))
+                      dataTableOutput("max")), 
+             #Feature 2
+             tabPanel("Sector Performance", 
+                     radioButtons("sec", label = "select a sector",
+                                   choices = unique(stocks$gics_sector) 
+                      ),
+                      dateRangeInput("d", label= "insert a date", 
+                                     start=min(stocks$date), 
+                                     end=max(stocks$date)),
+                      dataTableOutput("tab2")),
+             #Feature 3
+             tabPanel("Compare Multiple Secotrs",
+                      radioButtons("sec", label = "select a sector",
+                                   choices = unique(stocks$gics_sector) 
+                      ),
+                      dateRangeInput("d", label= "insert a date", 
+                                     start=min(stocks$date), 
+                                     end=max(stocks$date)),
+                      dataTableOutput("tab2")),
+             #Feature 4
+             tabPanel("Calculated Profit/Loss",
+                      numericInput("num", label = "How much money would I have if I bought this many shares", value = 1),
+                      dateRangeInput("date", label= "Buying and selling on these dates", 
+                                     start=min(stocks$date), 
+                                     end=max(stocks$date)),
+                      textInput("text", 
+                                label = "Of this company", 
+                                value = "Netflix Inc."),
+                      textOutput("tab4")))
 )
+
+server <- function(input, output){
+  output$max <- renderDataTable({
+    first_date <- input$dates[1]
+    last_date <- input$dates[2]
+    first_day <- stocks %>%
+      filter(date == first_date)
+    last_day <- stocks %>%
+      filter(date == last_date)
+    first_last_day <- first_day %>%
+      inner_join(
+        last_day,
+        by = "symbol",
+        suffix = c("First", "Last"))
+    f <- first_last_day %>%
+      mutate(PercentChange = (openLast - openFirst) / openFirst) %>%
+      mutate(PercentChangeStr = paste0(round(PercentChange * 100, 1), "%")) %>%
+      select(symbol, openFirst, openLast, PercentChange, PercentChangeStr)
+    
+    f<-as.data.frame(f[which.max(f$PercentChange),])
+    f
+    
+  })
+  
+  #Feature 2
+  output$tab2 <- renderDataTable({
+    p <- filter(stocks, stocks$gics_sector==input$sec)
+    first_date <- input$d[1]
+    last_date <- input$d[2]
+    first_day <- p %>%
+      filter(date == first_date)
+    last_day <- p %>%
+      filter(date == last_date)
+    first_last_day <- first_day %>%
+      inner_join(
+        last_day,
+        by = "symbol",
+        suffix = c("First", "Last"))
+    f <- first_last_day %>%
+      mutate(PercentChange = (openLast - openFirst) / openFirst) %>%
+      mutate(PercentChangeStr = paste0(round(PercentChange * 100, 1), "%")) %>%
+      select(symbol, openFirst, openLast, PercentChange, PercentChangeStr)
+    f<-as.data.frame(f[which.max(f$PercentChange),])
+    f
+  })
+  
+  #Feature 3
+  output$tab3 <- renderPlot({
+    ag <- aggregate(close~gics_sector+date, stocks, FUN = mean)
+    ag <- tsibble(ag, index=date, key = gics_sector)
+    ag <- filter(ag, ag$gics_sector%in%input$CheckGroup)
+    autoplot(ag)
+  })
+  
+  #Feature 4
+  output$tab4 <- renderPrint({
+    number <- stocks[stocks$security==input$text,]
+    first_day <- number[number$date==input$date[1],]
+    last_day <- number[number$date==input$date[2],]
+    dolla <- (last_day$open*input$num)-(first_day$open*input$num)
+    paste("$", round(dolla,2))
+  })
+}
 
 shinyApp(ui,server)
 
